d3098d171deb6a338ca6744042abaa3559e3c23b
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-05-11 03:37:39 +0200

    Fix eslint error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import './main.css';
 
 const taskList = document.getElementById('task-list');
-const newTaskInput = document.getElementById('new-task-input');
-const newTask = document.querySelector('new-task');
-const btn = document.getElementById('btn'); 
+// const newTaskInput = document.getElementById('new-task-input');
+// const newTask = document.querySelector('new-task');
+// const btn = document.getElementById('btn'); 
 
 const tasks = [
   {
@@ -12,20 +12,19 @@ const tasks = [
     index: '0',
   },
   {
-      description: 'complete To Do list project',
-      completed: 'true',
-      index: '1',
+    description: 'complete To Do list project',
+    completed: 'true',
+    index: '1',
   },
-]
+];
 
 const createCheckbox = () => {
   const taskCheckbox = document.createElement('input');
   taskCheckbox.type = 'checkbox';
   return taskCheckbox;
-}
+};
 
 for (let index = 0; index < tasks.length; index += 1) {
-  console.log(tasks[index].description);
   const listItem = document.createElement('li');
   listItem.appendChild(createCheckbox());
   listItem.appendChild(document.createTextNode(tasks[index].description));
@@ -33,8 +32,7 @@ for (let index = 0; index < tasks.length; index += 1) {
 }
 
   // Create a horizontal line element
-  const hr = document.querySelector('.hr');
-  hr.innerHTML = `<hr>`;
-
-  const clear = document.getElementById('clear');
-  clear.innerHTML = `Clear all completed`;
+const hr = document.querySelector('.hr');
+hr.innerHTML = `<hr>`;
+const clear = document.getElementById('clear');
+clear.innerHTML = `Clear all completed`;
